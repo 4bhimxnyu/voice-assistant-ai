2eb9015e919a4b013ec57eb4096fe6ab7bcb5fb6
Abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2025-05-11 18:45:40 +0530

    this repository and project was a total disaster

--- voice_assistant.py
+++ voice_assistant.py
@@ -1,17 +1,15 @@
 import pyaudio
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sr #pip install SpeechRecognition
+import pyttsx3  # pip install pyttsx3
+import speech_recognition as sr  # pip install SpeechRecognition
 import datetime
-import wikipedia #pip install wikipedia
+import wikipedia  # pip install wikipedia
 import webbrowser
 import os
 import smtplib
 
-
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
-# print(voices[1].id)
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[0].id)  # You can change index if you want female voice
 
 
 def speak(audio):
@@ -21,20 +19,20 @@ def speak(audio):
 
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-    if hour>=0 and hour<12:
+    if hour >= 0 and hour < 12:
         speak("Good Morning!")
 
-    elif hour>=12 and hour<18:
+    elif hour >= 12 and hour < 18:
         speak("Good Afternoon!")
 
     else:
         speak("Good Evening!")
 
-    speak("I am Chat Boat. Please tell me how may I help you")
+    speak("I am Chat Bot. Please tell me how may I help you")
+
 
 def takeCommand():
-    #It takes microphone input from the user and returns string output
-
+    # It takes microphone input from the user and returns string output
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
@@ -45,28 +43,30 @@ def takeCommand():
         print("Recognizing...")
         query = r.recognize_google(audio, language='en-in')
         print(f"User said: {query}\n")
-
     except Exception as e:
-        # print(e)
         print("Say that again please...")
         return "None"
     return query
 
-def sendEmail(to, content):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('youremail@gmail.com', 'your-password')
-    server.sendmail('youremail@gmail.com', to, content)
-    server.close()
 
-if _name_ == "__main__":
+def sendEmail(to, content):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login('youremail@gmail.com', 'your-password')  # Make sure you use App Password for Gmail
+        server.sendmail('youremail@gmail.com', to, content)
+        server.close()
+    except Exception as e:
+        print("Failed to send email:", e)
+        speak("Sorry, I was unable to send the email.")
+
+
+if __name__ == "__main__":
     wishMe()
     while True:
-    # if 1:
         query = takeCommand().lower()
 
-        # Logic for executing tasks based on query
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
             query = query.replace("wikipedia", "")
@@ -76,35 +76,45 @@ if _name_ == "__main__":
             speak(results)
 
         elif 'open youtube' in query:
-            webbrowser.open("youtube.com")
+            webbrowser.open("https://www.youtube.com")
 
         elif 'open google' in query:
-            webbrowser.open("google.com")
+            webbrowser.open("https://www.google.com")
 
         elif 'open stackoverflow' in query:
-            webbrowser.open("stackoverflow.com")
+            webbrowser.open("https://stackoverflow.com")
 
         elif 'play music' in query:
-            music_dir = ''
-            songs = os.listdir(music_dir)
-            print(songs)
-            os.startfile(os.path.join(music_dir, songs[0]))
+            webbrowser.open("https://open.spotify.com/track/5HNCy40Ni5BZJFw1TKzRsC")  # Update this to your actual music folder
+            try:
+                songs = os.listdir(music_dir)
+                if songs:
+                    os.startfile(os.path.join(music_dir, songs[0]))
+                else:
+                    speak("No songs found in the directory.")
+            except Exception as e:
+                print("Error playing music:", e)
+                speak("Unable to play music.")
 
         elif 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"Sir, the time is {strTime}")
 
         elif 'open code' in query:
-            codePath = ""
-            os.startfile(codePath)
+            codePath = "C:\\Path\\To\\Your\\IDE.exe"  # Example: "C:\\Users\\YourName\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            try:
+                os.startfile(codePath)
+            except Exception as e:
+                print("Error opening code editor:", e)
+                speak("I couldn't open the code editor.")
 
         elif 'email to me' in query:
             try:
                 speak("What should I say?")
                 content = takeCommand()
-                to = "Xyz@gmail.com"
+                to = "youremail@gmail.com"  # Replace with your actual email
                 sendEmail(to, content)
                 speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("")
+                speak("Sorry, I couldn't send the email.")
